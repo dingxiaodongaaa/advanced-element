56f6c5a51a840b0d775719a9effaa6e5b468c5fa
dingxiaodongaaa <dingxiaodongaaa@163.com>
2020-03-17 17:45:58 +0800

    下拉框封装文件语义化

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
+      <router-link to="/select">下拉框封装</router-link>
     </div>
     <router-view/>
   </div>
